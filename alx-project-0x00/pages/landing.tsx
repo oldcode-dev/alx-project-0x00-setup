c7249fc2b48b3204081ada6cafbc61f0d212e790
oldcode-dev <ernestfrimpongopoku.992@outlook.com>
2025-09-21 14:11:54 +0000

    import Button into landing.jsx and add 3 instances

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,13 @@
 import Card from "@/components/Card"
-
+import Button from "@/components/Button"
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button title="Book a flat" styles="rounded-sm" />
+      <Button title="Book a flat" styles="rounded-lg" />
+      <Button title="Book a flat" styles="rounded-full" />
     </div>
   )
 }
